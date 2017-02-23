c732a4d859b255076b0a9dfa6417ad1191222445
zhaokefei <546109656@qq.com>
2017-02-23 22:27:27 +0800

    heruko ready

--- config.py
+++ config.py
@@ -5,6 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Hello]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <18292878089@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
@@ -13,11 +18,6 @@ class Config:
     FLASKY_COMMENTS_PER_PAGE = 30
     FLASKY_SLOW_DB_QUERY_TIME=0.5
     FLASKY_COMMENTS_PER_PAGE = 10
-
-    UPLOAD_FOLDER = os.path.join(basedir, 'app/static/')
-    UPLOADS_DEFAULT_DEST = os.path.join(basedir, 'flask/static/upload')
-    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-    MAX_CONTENT_LENGTH = 512 * 1024
 
     @staticmethod
     def init_app(app):
@@ -44,6 +44,29 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'postgresql://kefei:5461@localhost/blog'
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MIAL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + 'Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 
 config = {
     'development': DevelopmentConfig,
--- manage.py
+++ manage.py
@@ -2,8 +2,14 @@
 # -*- coding:utf-8 -*-
 
 import os
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Post, Comment, Follow, Permission
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -12,9 +18,46 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post,
+                Comment=Comment, Follow=Follow, Permission=Permission)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test(coverage=False):
+    """Run the unit test"""
+    if coverage and os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable]+sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print 'Coverage Summary:'
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print 'HTML version:file://%s/index.html' % covdir
+        COV.erase()
+
+@manager.command
+def deploy():
+    from flask.ext.migrate import upgrade
+    from app.models import Role, User
+
+    # 把数据库迁移到最新修订版本
+    upgrade()
+
+    # 创建用户角色
+    Role.insert_roles()
+
+    # 让所有用户都关注此用户
+    User.add_self_follows()
+
 
 if __name__ == '__main__':
     manager.run()
